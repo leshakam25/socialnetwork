4d08f51d05dcae68e615457b65a7451580751277
leshakam25 <leshakam25@gmail.com>
2021-12-23 02:29:51 +0300

    empty

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+
 let store = {
   _state: {
     profile: {
@@ -39,7 +42,7 @@ let store = {
   },
 
   dispatch(action) {
-    if (action.type === "ADD-POST") {
+    if (action.type === ADD_POST) {
       let newPost = {
         id: 5,
         message: this._state.profile.newPostText,
@@ -49,25 +52,19 @@ let store = {
       this._state.profile.postData.push(newPost);
       this._state.profile.newPostText = "";
       this._callSubscriber(this._state);
-    } else if (action.type === "UPDATE-NEW-POST-TEXT") {
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
       this._state.profile.newPostText = action.text;
       this._callSubscriber(this._state);
     }
   },
 };
 
-export const addPostActionCreator = () => {
-  return {
-    type: "ADD-POST",
-  };
-};
+export const addPostActionCreator = () => ({ type: ADD_POST });
 
-export const updateNewPostTextActionCreator = (text) => {
-  return {
-    type: "UPDATE-NEW-POST-TEXT",
-    text: text,
-  };
-};
+export const updateNewPostTextActionCreator = (text) => ({
+  type: UPDATE_NEW_POST_TEXT,
+  text: text,
+});
 
 export default store;
 window.store = store;
